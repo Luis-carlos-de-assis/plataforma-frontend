78a0420026fca8395065013c958f6a98e3202ff1
Luis-carlos-de-assis <luiscarlosdeassis65@gmail.com>
2026-01-11 00:26:56 -0300

    Criar GerenciarCardapio.js

---
+++ src/GerenciarCardapio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GerenciarCardapio() {
+    return (
+        <div>
+            <h1>Gerenciamento de Cardápio</h1>
+            <p>Em breve, aqui você poderá adicionar, editar e remover os itens do seu cardápio.</p>
+        </div>
+    );
+}
+
+export default GerenciarCardapio;
