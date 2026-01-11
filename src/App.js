807cc848010dbaaa4bbebd2590defa4d9eaf33ed
Luis-carlos-de-assis <luiscarlosdeassis65@gmail.com>
2026-01-11 11:46:15 -0300

    Set up routing with Login, Dashboard, and Cardapio

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import GerenciarCardapio from './pages/GerenciarCardapio';
 
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/cardapio" element={<GerenciarCardapio />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
