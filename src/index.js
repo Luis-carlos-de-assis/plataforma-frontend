64ac18f74949dd369d6de6cb9d494587946e1bd4
Luis-carlos-de-assis <luiscarlosdeassis65@gmail.com>
2026-01-11 14:37:23 -0300

    Refactor index.js to use App component
    Restaura index.js para carregar o componente App.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div style={{ padding: '50px', textAlign: 'center', fontSize: '24px' }}>
-      Olá, Mundo! O React está renderizando.
-    </div>
+    <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
