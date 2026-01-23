f3b17b998141e08f4d490d23c865fb1c7b84217a
Luis-carlos-de-assis <luiscarlosdeassis65@gmail.com>
2026-01-23 07:58:42 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pagina/Login';
+import Dashboard from './pagina/Dashboard';
+import Cardapio from './pagina/Cardapio';
+import './App.css';
+
+const PrivateRoute = ({ children }) => {
+    const token = localStorage.getItem('token');
+    return token ? children : <Navigate to="/" />;
+};
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+                <Route path="/dashboard/cardapio" element={<PrivateRoute><Cardapio /></PrivateRoute>} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
+      
